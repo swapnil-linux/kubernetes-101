d5fb2bef0965d1a99c1e2f21c517a8aec3f2d901
Swapnil Jain <swapnil@linux.com>
2022-07-03 12:31:49 +0000

    storage lab files

---
+++ labs/storage/friends.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS MyGuests (id INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL )  ENGINE=INNODB;
+insert into MyGuests (firstname,lastname) VALUES ("Chandler","Bing");
+insert into MyGuests (firstname,lastname) VALUES ("Phoebe","Buffay");
+insert into MyGuests (firstname,lastname) VALUES ("Dr. Ross","Geller");
+insert into MyGuests (firstname,lastname) VALUES ("Monica","Geller");
+insert into MyGuests (firstname,lastname) VALUES ("Rachel","Green");
+insert into MyGuests (firstname,lastname) VALUES ("Joey","Tribbiani Jr.");
